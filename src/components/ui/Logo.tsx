005eee85eaa57ca5acb9ba2335dcaa3fc5dd6a65
JonasSchweigler <swejoz19@htl-kaindorf.at>
2024-07-22 14:07:48 +0200

    fixed logo navigation

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+import Link from "next/link";
 
 export const Logo = () => {
   return (
-    <div className='flex items-center gap-2 flex-row'>
-      <Avatar className='w-6 h-6'>
+    <Link
+      className='flex items-center gap-2 flex-row hover:cursor-pointer select-none'
+      href={"/"}
+    >
+      <Avatar className='w-6 h-6 select-none'>
         <AvatarImage className='object-cover' src='/branding/logo.png' />
         <AvatarFallback>CT</AvatarFallback>
       </Avatar>
-      <p className='font-light'>Coustra</p>
-    </div>
+      <p className='font-light select-none'>Coustra</p>
+    </Link>
   );
 };
